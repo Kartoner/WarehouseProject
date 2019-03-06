5213eb283cadd184bb555334bc96b811360772e3
Mateusz Trynkiewicz <mateusz.trynkiewicz@outlook.com>
2019-03-06 19:02:06 +0100

    Pierwsza klasa

---
+++ src/main/java/BOT/Logic/WarehouseProduct.java
@@ -0,0 +1,32 @@
+package BOT.Logic;
+
+public class WarehouseProduct
+{
+    private long Id;
+    private String Name;
+    private String Description;
+
+    public long getId() {
+        return Id;
+    }
+
+    public void setId(long id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+}
