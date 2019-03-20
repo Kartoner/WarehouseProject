0227a0f81bf5ba3876aa2a3f128db2bae27eb725
Mateusz Trynkiewicz <mateusz.trynkiewicz@outlook.com>
2019-03-20 17:44:19 +0100

    Kolejne metody API

--- src/main/java/BOT/warehouseProject/Website/Controllers/WebController.java
+++ src/main/java/BOT/warehouseProject/Website/Controllers/WebController.java
@@ -2,6 +2,8 @@ package BOT.warehouseProject.Website.Controllers;
 
 import BOT.warehouseProject.Authentication.Entities.User;
 import BOT.warehouseProject.Authentication.Services.IUserService;
+import BOT.warehouseProject.Domain.Entities.Delivery;
+import BOT.warehouseProject.Domain.Entities.WarehouseItem;
 import BOT.warehouseProject.Domain.Services.IWarehouseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class WebController
@@ -35,10 +39,69 @@ public class WebController
             ///todo dorobić loggera
             return new ResponseEntity(HttpStatus.NOT_FOUND);
         }
-        return new ResponseEntity<User>(user,HttpStatus.OK);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/delivery/{id}", method = RequestMethod.GET)
+    public ResponseEntity<Delivery> getDelivery(@PathVariable("id")long id)
+    {
+        Delivery delivery = warehouseService.getDelivery(id);
+        if(delivery == null)
+        {
+            ///todo dorobić loggera
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(delivery, HttpStatus.OK);
     }
 
 
+    @RequestMapping(value = "/item/{id}", method = RequestMethod.GET)
+    public ResponseEntity<WarehouseItem> getWarehouseItem(@PathVariable("id")long id)
+    {
+        WarehouseItem warehouseItem = warehouseService.getWarehouseItem(id);
+        if(warehouseItem == null)
+        {
+            ///todo dorobić loggera
+            return new ResponseEntity(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(warehouseItem, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public ResponseEntity<List<User>> getAllUsers()
+    {
+        List<User> users = userService.getAllUsers();
+        if(users.isEmpty())
+        {
+            ///todo dorobić loggera
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/delivery", method = RequestMethod.GET)
+    public ResponseEntity<List<Delivery>> getAllDeliveries()
+    {
+        List<Delivery> deliveries = warehouseService.getAllDeliveries();
+        if(deliveries.isEmpty())
+        {
+            ///todo dorobić loggera
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(deliveries, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/item", method = RequestMethod.GET)
+    public ResponseEntity<List<WarehouseItem>> getAllItems()
+    {
+        List<WarehouseItem> items = warehouseService.getAllItems();
+        if(items.isEmpty())
+        {
+            ///todo dorobić loggera
+            return new ResponseEntity(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(items, HttpStatus.OK);
+    }
 
 
 }
