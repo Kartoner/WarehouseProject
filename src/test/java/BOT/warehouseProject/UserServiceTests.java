5e19b16d9395b4f7e76bfbaa779acd347eb364de
Mateusz Trynkiewicz <mateusz.trynkiewicz@outlook.com>
2019-03-27 00:06:06 +0100

    test cz2

--- src/test/java/BOT/warehouseProject/UserServiceTests.java
+++ src/test/java/BOT/warehouseProject/UserServiceTests.java
@@ -2,24 +2,36 @@ package BOT.warehouseProject;
 
 import BOT.warehouseProject.Authentication.Entities.User;
 import BOT.warehouseProject.Authentication.Enums.UserStatus;
+import BOT.warehouseProject.Authentication.Services.Implementations.UserServiceImplementation;
 import BOT.warehouseProject.Database.Repositories.UserRepository;
+import BOT.warehouseProject.Domain.Services.Implementations.WarehouseServiceImplementation;
+import BOT.warehouseProject.Website.Controllers.WebController;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
 
 import static org.mockito.Mockito.mock;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UserServiceTests
 {
     @Autowired
     private UserRepository usersRepository;
+    @Autowired
+    private MockMvc mockMvc;
 
 
     @Before
@@ -75,6 +87,48 @@ public class UserServiceTests
 
         ///Then
         assert(!(u1.getUsername().equals(u2.getUsername())));
+    }
 
+    @Test
+    public void contextLoad_Test()
+    {
+        assert(usersRepository!=null);
+    }
+
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+        this.mockMvc.perform(get("/user/0")).
+                andDo(print())
+                .andExpect(status().isOk());
+//                .andExpect(content().string(containsString("Hello World")));11
+
+        int z=3;
+    }
+
+
+
+
+    @Test
+    public void getAllUser_Test()
+    {
+        ///Given
+        List<User> users = usersRepository.findAll();
+
+        WarehouseServiceImplementation warehouseServiceImplementation
+                = new WarehouseServiceImplementation();
+        UserServiceImplementation userServiceImplementation
+                = new UserServiceImplementation();
+        WebController webController
+                = new WebController(userServiceImplementation,warehouseServiceImplementation);
+
+        ResponseEntity<List<User>> responseWithUser = webController.getAllUsers();
+
+        ///When
+        List<User> usersFromResponse = responseWithUser.getBody();
+
+        ///Then
+        assert(usersFromResponse != null);
+        assert(users.size() != usersFromResponse.size());
     }
 }
