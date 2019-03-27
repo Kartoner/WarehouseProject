3769b7496f0f881d29cdafe1dea8cb4c57da3d92
Kartoner <jakub.michal.ostrowski@gmail.com>
2019-03-27 11:40:17 +0100

    Poprawiono testy UserService

--- src/test/java/BOT/warehouseProject/UserServiceTests.java
+++ src/test/java/BOT/warehouseProject/UserServiceTests.java
@@ -2,30 +2,83 @@ package BOT.warehouseProject;
 
 import BOT.warehouseProject.Authentication.Entities.User;
 import BOT.warehouseProject.Authentication.Enums.UserStatus;
+import BOT.warehouseProject.Authentication.Services.IUserService;
+import BOT.warehouseProject.Authentication.Services.Implementations.UserServiceImplementation;
 import BOT.warehouseProject.Database.Repositories.UserRepository;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.stubbing.Answer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class UserServiceTests
 {
+    @TestConfiguration
+    static class UserServiceTestsConfiguration{
+        @Bean IUserService userServiceTest(){
+            return new UserServiceImplementation();
+        }
+    }
+
+    @MockBean
+    private UserRepository userRepositoryMock;
+
     @Autowired
-    private UserRepository usersRepository;
+    @Qualifier("userServiceTest")
+    private IUserService userService;
 
-
-    @Before
-    public void setUpUserRepository()
+    @Test
+    public void givenUserService_WhenGettingAllUsers_ThenCheckUserListSize()
     {
-        ///to można by było gdzies wyciagnać
+        when(userRepositoryMock.findAll()).thenReturn(this.givenAllUsers());
+        List<User> users = userService.getAllUsers();
+
+        assert(users.size() == 2);
+    }
+
+    @Test
+    public void givenUserId_WhenGetUser_thenReturnUser()
+    {
+        ///Given
+        Long testId = 0L;
+        User testCustomer = this.givenCustomerUser();
+
+        ///When
+        when(userRepositoryMock.getOne(argThat(aLong -> aLong.equals(testId))))
+                .thenAnswer((Answer) invocation -> testCustomer);
+        User customer = this.userService.getUser(testId);
+
+        ///Then
+        assert(customer == testCustomer);
+
+    }
+
+    private List<User> givenAllUsers(){
+        List <User> usersList = new ArrayList<>();
+
+        User customer = this.givenCustomerUser();
+        User employee = this.givenEmployeeUser();
+
+        usersList.add(customer);
+        usersList.add(employee);
+
+        return usersList;
+    }
+
+    private User givenCustomerUser(){
         String customerName = "customer";
         String customerSecondName = "customer secondName";
         String customerPassword = "customer password";
@@ -33,48 +86,28 @@ public class UserServiceTests
         String customerEmail = "customer email";
         String customerPhoneNumber = "customer phoneNumber";
         UserStatus customerStatus = UserStatus.Customer;
-        ///given
+
         User customer = new User(customerName,
                 customerPassword, customerStatus,
                 customerName, customerSecondName, customerAddress,
                 customerEmail, customerPhoneNumber);
 
-        String employee = "emplyee";
-        String employeeSecondName = "emplyee secondName";
-        String employeePassword = "emplyee  password";
-        String employeeAddress = "emplyee address";
-        String employeeEmail = "emplyee email";
-        String employeePhoneNumber = "emplyee phoneNumber";
-        UserStatus emplyeeStatus = UserStatus.Employee;
+        return customer;
+    }
 
-        User employeeUser = new User(employee,
-                employeePassword, emplyeeStatus,employee, employeeSecondName, employeeAddress,
+    private User givenEmployeeUser(){
+        String employeeName = "employee";
+        String employeeSecondName = "employee secondName";
+        String employeePassword = "employee  password";
+        String employeeAddress = "employee address";
+        String employeeEmail = "employee email";
+        String employeePhoneNumber = "employee phoneNumber";
+        UserStatus employeeStatus = UserStatus.Employee;
+
+        User employee = new User(employeeName,
+                employeePassword, employeeStatus,employeeName, employeeSecondName, employeeAddress,
                 employeeEmail, employeePhoneNumber);
 
-        usersRepository.save(customer);
-        usersRepository.save(employeeUser);
-        usersRepository.flush();
-    }
-
-    @Test
-    public void checkIfSetupProceedSuccessfully()
-    {
-        List<User> users = usersRepository.findAll();
-        assert(users.size() > 1);
-    }
-
-    @Test
-    public void whenFindByName_thenReturnUser()
-    {
-        ///Given
-        List<User> users = usersRepository.findAll();
-
-        ///when
-        User u1 = users.get(0);
-        User u2 = users.get(1);
-
-        ///Then
-        assert(!(u1.getUsername().equals(u2.getUsername())));
-
+        return employee;
     }
 }
